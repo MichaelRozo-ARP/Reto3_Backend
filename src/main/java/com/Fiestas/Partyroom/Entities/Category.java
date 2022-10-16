bd2e5887f79f194534efad8960d8843849b18ce2
DavidRP111 <mdavidrozo1@hotmail.com>
2022-10-16 13:48:51 -0500

    agrego front end y reto 5 back end

--- src/main/java/com/Fiestas/Partyroom/Entities/Category.java
+++ src/main/java/com/Fiestas/Partyroom/Entities/Category.java
@@ -1,7 +1,6 @@
 package com.Fiestas.Partyroom.Entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
